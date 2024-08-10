62e7aee08e4f0de90f50ae2fa61ef8c4d43696be
ppaauuoo <60506291+ppaauuoo@users.noreply.github.com>
2024-08-10 16:50:01 +0700

    Revert "Kan 18 feature prefer nearest deadline first"

--- order_optimization/urls.py
+++ order_optimization/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.order_optimizer_view, name='order_optimizer_view'),
-    path('progress/', views.progress_view, name='progress_view'),
-    path('file_selector/', views.file_selector_view, name='file_selector_view'),
+    path('', views.optimize_order, name='optimize_order'),
+    path('get_progress/', views.get_progress, name='get_progress'),
 ]
