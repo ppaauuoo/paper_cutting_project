dc05a3a88741c713378c78006af3ed03a53e1429
ppaauuoo <bcg.channel@gmail.com>
2024-08-30 15:29:56 +0700

    test(new test for cache retriever)

--- order_optimization/test/test_getter.py
+++ order_optimization/test/test_getter.py
@@ -9,17 +9,16 @@ from order_optimization.getter import get_orders_cache, set_orders_model
 from order_optimization.models import CSVFile
 
 
+# Retrieves orders from cache when available
 @pytest.mark.django_db
-def test_orders_cache(mocker):
-    file_id = '1'
+def test_retrieves_orders_from_cache(mocker):
+    file_id = "test_file_id"
     expected_orders = pd.DataFrame(
         {"order_number": [1, 2], "due_date": ["01/01/23", "02/01/23"]}
     )
-    mock_csv = mocker.Mock()
-    mock_csv.file.path = "test_path"
 
-    mocker.patch("order_optimization.getter.get_csv_file", return_value=mock_csv)
     mocker.patch("django.core.cache.cache.get", return_value=expected_orders)
+    mocker.patch("order_optimization.getter.PLAN_RANGE", 1)
 
     orders = get_orders_cache(file_id)
 
@@ -29,7 +28,7 @@ def test_orders_cache(mocker):
 
 @pytest.mark.django_db
 def test_orders_nocache(mocker):
-    file_id = '1'
+    file_id = "1"
     expected_orders = pd.DataFrame(
         {"order_number": [1, 2], "due_date": ["01/01/23", "02/01/23"]}
     )
@@ -44,14 +43,11 @@ def test_orders_nocache(mocker):
         {"order_number": 2, "due_date": "02/01/23"},
     ]
 
-
-
     mocker.patch("django.core.cache.cache.get", return_value=None)
     mocker.patch("order_optimization.getter.get_csv_file", return_value=mock_csv)
     mocker.patch(
         "order_optimization.models.OrderList.objects.filter", return_value=mock_queryset
     )
-    
 
     orders = get_orders_cache(file_id)
 
