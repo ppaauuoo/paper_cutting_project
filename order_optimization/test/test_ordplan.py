e4f7613d85712385459cbb7136058e67d841393c
ppaauuoo <bcg.channel@gmail.com>
2024-09-06 08:58:17 +0700

    test(common)

--- order_optimization/test/test_ordplan.py
+++ order_optimization/test/test_ordplan.py
@@ -83,7 +83,36 @@ def test_selected_order(test_data):
 
 
 @pytest.mark.django_db
-def test_common_order(test_data):
+def test_common_order():
+    test_data = pd.DataFrame(
+        {
+            "id": [1, 2, 3, 4, 5, 6, 7],
+            "width": [66.04, 66.04, 66.04, 66.04, 80, 80, 0.0],
+            "length": [200.0, 200.0, 200.0, 200.0, 200.0, 100.0, 0],
+            "due_date": [
+                "08/01/23",
+                "08/01/23",
+                "08/05/23",
+                "08/10/23",
+                "08/15/23",
+                "08/20/23",
+                "02/21/22",
+            ],
+            "front_sheet": [1, 1, 1, 1, 1, 1, 0],
+            "c_wave": [1, 2, 1, 1, 1, 1, 0],
+            "middle_sheet": [1, 1, 1, 1, 1, 1, 0],
+            "b_wave": [1, 1, 1, 1, 1, 1, 0],
+            "back_sheet": [1, 1, 1, 1, 1, 1, 0],
+            "level": [1, 1, 1, 1, 1, 1, 0],
+            "edge_type": [1, 1, 1, 1, 1, 1, 0],
+            "left_edge_cut": [1, 1, 1, 1, 1, 1, 0],
+            "middle_edge_cut": [1, 1, 2, 1, 1, 1, 0],
+            "right_edge_cut": [1, 1, 1, 1, 1, 1, 0],
+            "component_type": [1, 1, 1, 1, 1, 1, 0],
+            "quantity": [1, 1, 1, 1, 1, 1, 1],
+        }
+    )
+
     ord = ORD(test_data, size=66, common=True, no_build=True)
     ord.format_data()
     ord.filter_common_order()
