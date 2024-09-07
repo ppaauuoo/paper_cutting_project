bd6c537d830c530a0e1d460e9c14322c10540d29
ppaauuoo <bcg.channel@gmail.com>
2024-09-07 13:08:19 +0700

    ops: add try except block

--- order_optimization/controller.py
+++ order_optimization/controller.py
@@ -1,12 +1,15 @@
 from pyparsing import str_type
 from order_optimization.getter import get_orders
 from order_optimization.handler import handle_auto_config, handle_saving
-
+from django.contrib import messages
 
 def optimizer_controller(request)->None:
     for i in range(0,5):
-        handle_auto_config(request)
-        handle_saving(request)
+        try:
+            handle_auto_config(request)
+            handle_saving(request)
+        except(ValueError):
+            messages.error(request, "There's no orders available.")
 
     
     
